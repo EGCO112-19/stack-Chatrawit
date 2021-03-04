8e9545ad82f74222ccd3fc864468c4fea46d300e
Chatrawit <Chatrawit.chat@gmail.com>
2021-03-04 16:34:46 +0000

    stack new

--- main.c
+++ main.c
@@ -8,17 +8,52 @@ int main(int argc, char **argv){
   int i,N,j;
   
   Stack s;
+  s.size = 0;
+  s.top = NULL;
+  char popvalue;
+  int check=0;
 
 
- 
-  for(i=1;i<argc;i++){
-   
+  for(i=1;i<argc;i++){// each argument
+    //printf("%s\n",argv[i]);
+    //printf("%ld\n",strlen(argv[i]));
      for(j=0;j<strlen(argv[i]);j++){
        /* Use stack to help with the parentheses*/
-
-
+        if(argv[i][j] == '[' || argv[i][j] == '{'){
+          push(&s, argv[i][j]); 
+       }
+       else if (argv[i][j] == ']'){
+          if (s.size==0){
+            check = 1;
+            break;
+          }
+          popvalue = pop(&s);
+          if (popvalue != '['){
+            check = 1;
+            break;
+          }
+       }
+       else if (argv[i][j] == '}'){
+         if (s.size==0){
+           check = 1;
+            break;
+          }
+          popvalue = pop(&s);
+          if (popvalue != '{'){
+            check = 1;
+            break;
+          }
+       }
+      
      }
-
+if(s.size>0 || check != 0){
+        //printf("%d ..\n",s.size);
+         printf("The parenthses do not match for %s \n", argv[i]);
+         pop_all(&s);
+       }else{
+        // printf("%d ..\n",s.size);
+         printf("The parenthses match successfully for %s \n", argv[i]);
+       }
 
   }
 
@@ -27,3 +62,17 @@ int main(int argc, char **argv){
 
    return 0;
 }
+
+ /*switch(argv[i][j]){
+         case '[':
+            break;
+         case '{':
+            push(&s, argv[i][j]);
+            break;
+         case ']':
+            
+            break;
+         case '}':
+            pop(&s);
+            break;
+       }*/ 
